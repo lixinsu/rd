20cd678730eee2420604b464ec57d46a346db6ca
xyzhang16 <404338476@qq.com>
2018-08-21 20:49:10 +0800

    .
    Signed-off-by: xyzhang16 <404338476@qq.com>

--- config/config_r_net.py
+++ config/config_r_net.py
@@ -6,9 +6,9 @@ from config import config_base
 
 class Config(config_base.ConfigBase):
     model_name = 'r_net'
-    model_save = model_name + '_3_1'
-    is_bn = False
-    epoch = 10
+    model_save = model_name + '_2'
+    is_bn = True
+    epoch = 12
     mode = 'GRU'
     batch_size = 32
     hidden_size = 75
